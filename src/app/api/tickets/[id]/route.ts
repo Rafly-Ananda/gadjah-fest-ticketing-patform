94ef64ab5ce517202de87b6b93b5d3bf36c2a0b3
rafly-ananda <anandarafly15@gmail.com>
2023-08-01 23:57:55 +0700

    new method nodemailer

--- src/app/api/tickets/[id]/route.ts
+++ src/app/api/tickets/[id]/route.ts
@@ -2,43 +2,81 @@ import { NextResponse } from "next/server";
 import { prismaClientInstance } from "@/_base";
 import axios from "axios";
 const prisma = prismaClientInstance;
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer";
+// const nodemailer = require("nodemailer");
 
-async function sendMail() {
-  console.log(process.env.NODEMAILER_EMAIL);
-  console.log(process.env.NODEMAILER_PW);
+type EmailPayload = {
+  to: string;
+  subject: string;
+  html?: string;
+};
+
+// Replace with your SMTP credentials
+const smtpOptions = {
+  host: process.env.SMTP_HOST || "smtp.mailtrap.io",
+  port: parseInt(process.env.SMTP_PORT || "2525"),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER || "user",
+    pass: process.env.SMTP_PASSWORD || "password",
+  },
+};
+
+// async function sendMail() {
+//   console.log(process.env.NODEMAILER_EMAIL);
+//   console.log(process.env.NODEMAILER_PW);
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     secureConnection: true,
+//     auth: {
+//       user: process.env.NODEMAILER_EMAIL,
+//       pass: process.env.NODEMAILER_PW,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: process.env.NODEMAILER_EMAIL,
+//     to: "kayzeel15@gmail.com",
+//     subject: "Testozz",
+//     text: "http://localhost:3000/",
+//   };
+
+//   transporter.sendMail(mailOptions, (error: any, info: any) => {
+//     if (error) {
+//       console.log(error);
+//       console.log("err occurred");
+//     } else {
+//       console.log("email sento");
+//       return true;
+//     }
+//   });
+// }
+
+export const sendEmail = async (data: EmailPayload) => {
   const transporter = nodemailer.createTransport({
+    // ...smtpOptions,
     service: "gmail",
-    secureConnection: true,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.NODEMAILER_EMAIL,
       pass: process.env.NODEMAILER_PW,
     },
   });
 
-  const mailOptions = {
-    from: process.env.NODEMAILER_EMAIL,
-    to: "kayzeel15@gmail.com",
-    subject: "Testozz",
-    text: "http://localhost:3000/",
-  };
-
-  transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if (error) {
-      console.log(error);
-      console.log("err occurred");
-    } else {
-      console.log("email sento");
-      return true;
-    }
+  return await transporter.sendMail({
+    from: process.env.SMTP_FROM_EMAIL,
+    ...data,
+    text: "aiueo",
   });
-}
+};
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
 ) {
-  await sendMail();
+  await sendEmail({ to: "kayzeel15@gmail.com", subject: "Welcome to NextAPI" });
 
   return NextResponse.json({
     status: "Success",
