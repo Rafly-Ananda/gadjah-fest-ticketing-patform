64cc47e8b4401673a0c08643e70c7270858a4c0b
rafly-ananda <anandarafly15@gmail.com>
2023-08-03 20:01:30 +0700

    change target host to prod

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const PROJECT_HOST = "http://localhost:3000";
-// export const PROJECT_HOST = "https://www.gadjahfest.com";
+// export const PROJECT_HOST = "http://localhost:3000";
+export const PROJECT_HOST = "https://www.gadjahfest.com";
