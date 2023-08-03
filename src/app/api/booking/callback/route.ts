3de3d84ae7c303e405d5b8487742c4216e445b7f
rafly-ananda <anandarafly15@gmail.com>
2023-08-03 11:30:40 +0700

    add trycatch to json parse obj on request in xendit callback

--- src/app/api/booking/callback/route.ts
+++ src/app/api/booking/callback/route.ts
@@ -54,26 +54,181 @@ const sendEmail = async (data: EmailPayloatInterface) => {
 export async function POST(
   request: Request,
 ) {
-  const body = await request.json();
+  try {
+    console.log("coming from callback");
+    const body = await request.json();
+    console.log(body);
 
-  console.log("callback masuk");
+    if (body.status === "PAID") {
+      try {
+        // ** 1 Update booking status
+        const updateBooking = await prisma.booking.update({
+          where: {
+            id: body.external_id,
+          },
+          data: {
+            bookingStatus: "PAID",
+            payment: {
+              update: {
+                where: {
+                  bookingId: body.external_id,
+                },
+                data: {
+                  status: "PAID",
+                },
+              },
+            },
+            bookingDetails: {
+              updateMany: {
+                where: {
+                  bookingId: body.external_id,
+                },
+                data: {
+                  itemStatus: "PAID",
+                },
+              },
+            },
+          },
+          include: {
+            bookingDetails: {
+              where: {
+                bookingId: body.external_id,
+              },
+            },
+            user: true,
+          },
+        });
 
-  if (body.status === "PAID") {
-    try {
-      // ** 1 Update booking status
+        // ** 2 Generate ticket for QR
+        let purchasedTicketObj = [];
+        for (let i = 0; i < updateBooking.bookingDetails.length; i++) {
+          for (let j = 0; j < updateBooking.bookingDetails[i].quantity; j++) {
+            purchasedTicketObj.push({
+              masterTicketId: updateBooking.bookingDetails[i].masterTicketId,
+              ticketStatus: "VALID" as TicketStatus,
+              bookingId: updateBooking.bookingDetails[i].bookingId,
+              marathonDetail: "",
+            });
+          }
+        }
+
+        await prisma.purchasedTicket.createMany({
+          data: [...purchasedTicketObj],
+        });
+
+        const generatedTickets = await prisma.purchasedTicket.findMany({
+          where: {
+            bookingId: updateBooking.id,
+          },
+        });
+
+        // ** 3 Save to S3
+        const generateQR = async (text: any) => {
+          try {
+            return QRCode.toDataURL(text);
+          } catch (err) {
+            console.error(err);
+          }
+        };
+
+        for (const ticket of generatedTickets) {
+          const qrCode = await generateQR(JSON.stringify(ticket));
+          const base64Img = new (Buffer as any).from(
+            qrCode?.replace(/^data:image\/\w+;base64,/, ""),
+            "base64",
+          );
+          const type = qrCode!.split(";")[0].split("/")[1];
+          await uploadQRCodetoS3(ticket.id, base64Img, type);
+          await prisma.purchasedTicket.update({
+            where: {
+              id: ticket.id,
+            },
+            data: {
+              s3BarcodeKeyUrl:
+                `https://gadjah-ticketing-platform.s3.ap-southeast-1.amazonaws.com/${ticket.id}.${type}`,
+            },
+          });
+        }
+
+        // ** 4  generate pdf to S3
+        await generatePdf(
+          updateBooking.id,
+          `${process.env.PROJECT_HOST}/invoice/${updateBooking.generatedBookingCode}`,
+        );
+
+        await prisma.booking.update({
+          where: {
+            id: body.external_id,
+          },
+          data: {
+            invoicePdfUrl:
+              `https://gadjah-ticketing-platform.s3.ap-southeast-1.amazonaws.com/${updateBooking.id}.pdf`,
+          },
+        });
+
+        const purchasedTickets = await prisma.purchasedTicket.findMany({
+          where: {
+            bookingId: updateBooking.id,
+          },
+          include: {
+            ticket: true,
+          },
+        });
+
+        // ** 6 Send Finish Payment Email
+        await sendEmail({
+          to: updateBooking.user.email,
+          subject: "Konfirmasi Pembayaran Gadjah Fest 2023",
+          bookingId: updateBooking.generatedBookingCode,
+          firstName: updateBooking.user.firstName,
+          attachmentLink:
+            `https://gadjah-ticketing-platform.s3.ap-southeast-1.amazonaws.com/${updateBooking.id}.pdf`,
+          tickets: [...purchasedTickets],
+        });
+
+        return NextResponse.json({
+          status: "Success",
+          message: "Payment received",
+          detail: {
+            id: updateBooking.id,
+            bookingId: updateBooking.generatedBookingCode,
+            userId: updateBooking.userId,
+            createdAt: updateBooking.createdAt,
+            updatedAt: updateBooking.updatedAt,
+          },
+        }, {
+          status: 200,
+        });
+      } catch (e) {
+        if (e instanceof Error) {
+          return NextResponse.json({
+            status: "Failed",
+            message: "Payment Received But Failed",
+            detail: {
+              ...e,
+            },
+          }, {
+            status: 500,
+          });
+        }
+      }
+    }
+
+    if (body.status === "EXPIRED") {
+      // 1 Update booking status
       const updateBooking = await prisma.booking.update({
         where: {
           id: body.external_id,
         },
         data: {
-          bookingStatus: "PAID",
+          bookingStatus: "EXPIRED",
           payment: {
             update: {
               where: {
                 bookingId: body.external_id,
               },
               data: {
-                status: "PAID",
+                status: "EXPIRED",
               },
             },
           },
@@ -83,7 +238,7 @@ export async function POST(
                 bookingId: body.external_id,
               },
               data: {
-                itemStatus: "PAID",
+                itemStatus: "REVOKED",
               },
             },
           },
@@ -94,100 +249,12 @@ export async function POST(
               bookingId: body.external_id,
             },
           },
-          user: true,
         },
-      });
-
-      // ** 2 Generate ticket for QR
-      let purchasedTicketObj = [];
-      for (let i = 0; i < updateBooking.bookingDetails.length; i++) {
-        for (let j = 0; j < updateBooking.bookingDetails[i].quantity; j++) {
-          purchasedTicketObj.push({
-            masterTicketId: updateBooking.bookingDetails[i].masterTicketId,
-            ticketStatus: "VALID" as TicketStatus,
-            bookingId: updateBooking.bookingDetails[i].bookingId,
-            marathonDetail: "",
-          });
-        }
-      }
-
-      await prisma.purchasedTicket.createMany({
-        data: [...purchasedTicketObj],
-      });
-
-      const generatedTickets = await prisma.purchasedTicket.findMany({
-        where: {
-          bookingId: updateBooking.id,
-        },
-      });
-
-      // ** 3 Save to S3
-      const generateQR = async (text: any) => {
-        try {
-          return QRCode.toDataURL(text);
-        } catch (err) {
-          console.error(err);
-        }
-      };
-
-      for (const ticket of generatedTickets) {
-        const qrCode = await generateQR(JSON.stringify(ticket));
-        const base64Img = new (Buffer as any).from(
-          qrCode?.replace(/^data:image\/\w+;base64,/, ""),
-          "base64",
-        );
-        const type = qrCode!.split(";")[0].split("/")[1];
-        await uploadQRCodetoS3(ticket.id, base64Img, type);
-        await prisma.purchasedTicket.update({
-          where: {
-            id: ticket.id,
-          },
-          data: {
-            s3BarcodeKeyUrl:
-              `https://gadjah-ticketing-platform.s3.ap-southeast-1.amazonaws.com/${ticket.id}.${type}`,
-          },
-        });
-      }
-
-      // ** 4  generate pdf to S3
-      await generatePdf(
-        updateBooking.id,
-        `${process.env.PROJECT_HOST}/invoice/${updateBooking.generatedBookingCode}`,
-      );
-
-      await prisma.booking.update({
-        where: {
-          id: body.external_id,
-        },
-        data: {
-          invoicePdfUrl:
-            `https://gadjah-ticketing-platform.s3.ap-southeast-1.amazonaws.com/${updateBooking.id}.pdf`,
-        },
-      });
-
-      const purchasedTickets = await prisma.purchasedTicket.findMany({
-        where: {
-          bookingId: updateBooking.id,
-        },
-        include: {
-          ticket: true,
-        },
-      });
-
-      // ** 6 Send Finish Payment Email
-      await sendEmail({
-        to: updateBooking.user.email,
-        subject: "Konfirmasi Pembayaran Gadjah Fest 2023",
-        bookingId: updateBooking.generatedBookingCode,
-        firstName: updateBooking.user.firstName,
-        attachmentLink:
-          `https://gadjah-ticketing-platform.s3.ap-southeast-1.amazonaws.com/${updateBooking.id}.pdf`,
-        tickets: [...purchasedTickets],
       });
 
       return NextResponse.json({
         status: "Success",
-        message: "Payment received",
+        message: "Payment expired",
         detail: {
           id: updateBooking.id,
           bookingId: updateBooking.generatedBookingCode,
@@ -198,71 +265,18 @@ export async function POST(
       }, {
         status: 200,
       });
-    } catch (e) {
-      if (e instanceof Error) {
-        return NextResponse.json({
-          status: "Failed",
-          message: "Payment Received But Failed",
-          detail: {
-            ...e,
-          },
-        }, {
-          status: 500,
-        });
-      }
     }
-  }
-
-  if (body.status === "EXPIRED") {
-    // 1 Update booking status
-    const updateBooking = await prisma.booking.update({
-      where: {
-        id: body.external_id,
-      },
-      data: {
-        bookingStatus: "EXPIRED",
-        payment: {
-          update: {
-            where: {
-              bookingId: body.external_id,
-            },
-            data: {
-              status: "EXPIRED",
-            },
-          },
+  } catch (e) {
+    if (e instanceof Error) {
+      return NextResponse.json({
+        status: "Error",
+        message: "Error Occurred",
+        detail: {
+          ...e,
         },
-        bookingDetails: {
-          updateMany: {
-            where: {
-              bookingId: body.external_id,
-            },
-            data: {
-              itemStatus: "REVOKED",
-            },
-          },
-        },
-      },
-      include: {
-        bookingDetails: {
-          where: {
-            bookingId: body.external_id,
-          },
-        },
-      },
-    });
-
-    return NextResponse.json({
-      status: "Success",
-      message: "Payment expired",
-      detail: {
-        id: updateBooking.id,
-        bookingId: updateBooking.generatedBookingCode,
-        userId: updateBooking.userId,
-        createdAt: updateBooking.createdAt,
-        updatedAt: updateBooking.updatedAt,
-      },
-    }, {
-      status: 200,
-    });
+      }, {
+        status: 500,
+      });
+    }
   }
 }
