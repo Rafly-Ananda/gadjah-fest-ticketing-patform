f58aaa7703408ed75c1e5172878529906c6bef7c
rafly-ananda <anandarafly15@gmail.com>
2023-08-03 11:34:25 +0700

    change project host

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // export const PROJECT_HOST = "http://localhost:3000";
-export const PROJECT_HOST = "https://gadjah-fest-ticket.vercel.app/";
+export const PROJECT_HOST = "https://www.gadjahfest.com/";
